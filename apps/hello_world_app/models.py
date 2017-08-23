fd0cbc3ac172761cfd9bf43c9d68db439fb5d958
GianFranco Macaraeg <gfmacaraeg@gmail.com>
2017-08-23 15:32:19 -0500

    Added more css hover and started database

--- apps/hello_world_app/models.py
+++ apps/hello_world_app/models.py
@@ -2,4 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class  Link(models.Model):
+	id = models.CharField(max_length=64, primary_key=True)
+	title = models.CharField(max_length=255)
+	link = models.CharField(max_length=255, blank=False)
+	subreddit = models.CharField(max_length=50)
+	nsfw = models.BooleanField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
--- apps/hello_world_app/views.py
+++ apps/hello_world_app/views.py
@@ -1,26 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 import praw
+
+
+
+def bot_login():
+	if 'reddit' not in request.session:
+		reddit = praw.Reddit(client_id='DBJ-wTtdIbMWfA',
+	                     client_secret="Yn8cgRnAbg1btB_n1VYNwx6z09k", password='codingdojo007',
+	                     user_agent='Student testing /u/cdojobot', username='cdojobot')
+		request.session['reddit'] = reddit
+	else:
+		reddit = request.session['reddit']	
+	return reddit
+
 
 def index(request):
 	image_url = []
-	image_data = {}
-	print praw
+	filetypes = []
+	
+	
 	reddit = praw.Reddit(client_id='DBJ-wTtdIbMWfA',
                      client_secret="Yn8cgRnAbg1btB_n1VYNwx6z09k", password='codingdojo007',
                      user_agent='Student testing /u/cdojobot', username='cdojobot')
-	print reddit
-	for submission in reddit.subreddit('pics').top('month', limit=100):
+	request.session['reddit'] = 0
+	# print reddit
+	for submission in reddit.subreddit('aww').top('all', limit=20):
     		# print(dir(submission))
-    		print submission.preview, submission.url
     		if "jpg" in submission.url:
     			image_url.append({'url':submission.url, 'title':submission.title})
-
+    		if submission.url[-4:] not in filetypes:
+    			filetypes.append(submission.url[-4:])
+    		print submission.id_from_url, " ", submission.id
 
     		# if submission.url[-4:] == "gifv":
     		# 	image_url.append(submission.url[:-1])
     		# else:
     		# 	image_url.append(submission.url)
-    	print image_url	
+    	print filetypes
     	context = {
     	'images': image_url
     	}
